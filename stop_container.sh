85dfc168a677281cca9eab91c107400021d7e195
saiGopireddi <113371128+saiGopireddi@users.noreply.github.com>
2024-05-08 18:32:12 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -4,7 +4,7 @@ set -e
 # Stop the running container (if any)
 containerid=$(docker ps | awk 'NR == 2 {print $1}')
 if [ ! -z "$containerid" ]; then
-    docker rm -f $containerid
+    docker rm -f container $containerid
 else
     echo "No running containers found. Skipping."
 fi
